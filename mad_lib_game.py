0899d71c8bd87d352b72c3e435b685df4a5599cf
Pacifique1234 <hakizimanapacifique2@gmail.com>
2022-04-07 05:04:56 -0700

    Add files via upload
    This file is simple madlibs game in Python

---
+++ mad_lib_game.py
@@ -0,0 +1,8 @@
+
+color=input("Enter Color: ")
+plural_noun=input("Enter plural noun: ")
+celebrity=input("Enter Celebrity: ")
+print(color+" are color")
+print(plural_noun+" are blue")
+print("I love "+celebrity)
+
