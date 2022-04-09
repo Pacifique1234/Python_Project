3619447627fac988c623fba9d1825d3930d837a7
Pacifique HAKIZIMANA <hakizimanapacifique2@gmail.com>
2022-04-09 08:08:07 -0700

    Add files via upload

---
+++ list_function.py
@@ -0,0 +1,4 @@
+friends=["Paccy","Emmy","Kelly","Happy","Yves","Kenny"]
+numbers=[1,2,3,4,5,6,7]
+friends2=friends.copy()
+print(friends2)
---
+++ tuple.py
@@ -0,0 +1,4 @@
+def itsok(name,ages):
+    print("Hello "+name+" you are "+ages)
+itsok("Mike","14")
+itsok("Sam","26")
